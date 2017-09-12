331d30b2f0174cbea108a253004ba218e77d1934
Martin Tournoij <martin@arp242.net>
2017-09-12 09:43:46 +0100

    Make it a bit more idiomatic Python

--- sanitize_files.py
+++ sanitize_files.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#encoding: utf-8
+# encoding: utf-8
 #
 # Copyright © 2014-2016 Martin Tournoij <martin@arp242.net>
 # See below for full copyright
@@ -8,188 +8,197 @@
 #
 
 from __future__ import print_function
-import argparse, fnmatch, os, re, sys
+import argparse
+import fnmatch
+import os
+import sys
 
 _verbose = False
 
 
 def verbose(msg):
-	if _verbose: print(msg)
+    if _verbose:
+        print(msg)
 
 
 def is_binary(data):
-	return data.find(b'\000') >= 0
+    return data.find(b'\000') >= 0
 
 
 # TODO: we probably also want to process ignore files, like in:
 # https://github.com/ggreer/the_silver_searcher/blob/master/src/ignore.c
 def should_always_ignore(path):
-	keep = [
-		# VCS systems
-		'/.git', '/.hg', '/.svn', '/CVS',
+    keep = [
+        # VCS systems
+        '/.git', '/.hg', '/.svn', '/CVS',
 
-		# These files have significant whitespace/tabs, and cannot be edited
-		# safely
-		# TODO: there are probably more of these files..
-		'Makefile', 'BSDmakefile', 'GNUmakefile', 'Gemfile.lock'
-	]
+        # These files have significant whitespace/tabs, and cannot be edited
+        # safely
+        # TODO: there are probably more of these files..
+        'Makefile', 'BSDmakefile', 'GNUmakefile', 'Gemfile.lock'
+    ]
 
-	for k in keep:
-		if k in path:
-			return True
-	return False
+    for k in keep:
+        if k in path:
+            return True
+    return False
 
 
 def run(files, indent_type='tabs', indent_width=4, max_newlines=2):
-	if indent_type == 'tabs':
-		indent_find = b' ' * indent_width
-		indent_replace = b'\t'
-	else:
-		indent_find = b'\t'
-		indent_replace = b' ' * indent_width
+    if indent_type == 'tabs':
+        indent_find = b' ' * indent_width
+        indent_replace = b'\t'
+    else:
+        indent_find = b'\t'
+        indent_replace = b' ' * indent_width
 
-	for f in files:
-		try:
-			size = os.stat(f).st_size
-		# Unresolvable symlink, just ignore those
-		except FileNotFoundError as exc:
-			verbose('%s is unresolvable, skipping (%s)' % (f, exc))
-			continue
+    for f in files:
+        try:
+            size = os.stat(f).st_size
+        # Unresolvable symlink, just ignore those
+        except FileNotFoundError as exc:
+            verbose('%s is unresolvable, skipping (%s)' % (f, exc))
+            continue
 
-		if size == 0: continue
-		if size > 1024 ** 2:
-			verbose("Skipping `%s' because it's over 1MiB" % f)
-			continue
+        if size == 0:
+            continue
+        if size > 1024 ** 2:
+            verbose("Skipping `%s' because it's over 1MiB" % f)
+            continue
 
-		try:
-			original = open(f, 'rb').read()
-		except (OSError, PermissionError) as exc:
-			print("Error: Unable to read `%s': %s" % (f, exc))
-			continue
+        try:
+            original = open(f, 'rb').read()
+        except (OSError, PermissionError) as exc:
+            print("Error: Unable to read `%s': %s" % (f, exc))
+            continue
 
-		if is_binary(original):
-			verbose("Skipping `%s' because it looks binary" % f)
-			continue
+        if is_binary(original):
+            verbose("Skipping `%s' because it looks binary" % f)
+            continue
 
-		new = original.split(b'\n')
+        new = original.split(b'\n')
 
-		was_dos = False
-		fixed_indent = False
-		lines_trimmed = 0
-		newlines_trimmed = 0
-		consec_lines = 0
-		for i, line in enumerate(new):
-			# Fix \r\n
-			if line[-1:] == b'\r':
-				was_dos = True
-				line = line[:-1]
+        was_dos = False
+        fixed_indent = False
+        lines_trimmed = 0
+        newlines_trimmed = 0
+        consec_lines = 0
+        for i, line in enumerate(new):
+            # Fix \r\n
+            if line[-1:] == b'\r':
+                was_dos = True
+                line = line[:-1]
 
-			# Trim whitespace
-			l = len(line)
-			line = line.rstrip()
-			if len(line) != l: lines_trimmed += 1
+            # Trim whitespace
+            l = len(line)
+            line = line.rstrip()
+            if len(line) != l:
+                lines_trimmed += 1
 
-			# Fix indentation
-			repl_count = 0
-			while line.startswith(indent_find):
-				fixed_indent = True
-				repl_count += 1
-				line = line.replace(indent_find, b'', 1)
+            # Fix indentation
+            repl_count = 0
+            while line.startswith(indent_find):
+                fixed_indent = True
+                repl_count += 1
+                line = line.replace(indent_find, b'', 1)
 
-			if repl_count > 0:
-				line = indent_replace * repl_count + line
+            if repl_count > 0:
+                line = indent_replace * repl_count + line
 
-			# Trim newlines
-			if line == b'':
-				consec_lines += 1
-			else:
-				consec_lines = 0
+            # Trim newlines
+            if line == b'':
+                consec_lines += 1
+            else:
+                consec_lines = 0
 
-			#print(max_newlines, consec_lines, line)
-			if consec_lines > max_newlines:
-				new[i] = None
-				newlines_trimmed += 1
-			else:
-				new[i] = line
+            if consec_lines > max_newlines:
+                new[i] = None
+                newlines_trimmed += 1
+            else:
+                new[i] = line
 
-		new = list(filter(lambda x: x is not None, new))
-		if was_dos: verbose('Fixed \\r\\n line endings')
-		if fixed_indent: verbose('Fixed indentation')
-		if lines_trimmed > 0: verbose('Trimmed trailing whitespace of %s lines' % lines_trimmed)
-		if newlines_trimmed > 0: verbose('Removed %s newlines' % newlines_trimmed)
-		if new[-1:] != [b'']:
-			verbose('Adding newline at end of file')
-			new += [b'']
+        new = list(filter(lambda x: x is not None, new))
+        if was_dos:
+            verbose('Fixed \\r\\n line endings')
+        if fixed_indent:
+            verbose('Fixed indentation')
+        if lines_trimmed > 0:
+            verbose('Trimmed trailing whitespace of %s lines' % lines_trimmed)
+        if newlines_trimmed > 0:
+            verbose('Removed %s newlines' % newlines_trimmed)
+        if new[-1:] != [b'']:
+            verbose('Adding newline at end of file')
+            new += [b'']
 
-		new = b'\n'.join(new)
-		if new != original:
-			verbose('Writing %s' % f)
-			try:
-				open(f, 'wb').write(new)
-			except:
-				print("Error: Unable to write to `%s': %s" % (f, sys.exc_info()[1]))
+        new = b'\n'.join(new)
+        if new != original:
+            verbose('Writing %s' % f)
+            try:
+                open(f, 'wb').write(new)
+            except:
+                print("Error: Unable to write to `%s': %s" % (f, sys.exc_info()[1]))
 
 
 def do_add_file(path):
-	if should_always_ignore(path):
-		verbose('Ignoring %s' % path)
-		return False
+    if should_always_ignore(path):
+        verbose('Ignoring %s' % path)
+        return False
 
-	for exclude in excludes:
-		if fnmatch.fnmatch(path, exclude):
-			verbose('Excluding %s' % path)
-			return False
+    for exclude in excludes:
+        if fnmatch.fnmatch(path, exclude):
+            verbose('Excluding %s' % path)
+            return False
 
-	return True
+    return True
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('-V', '--verbose', action='store_true',
-		help='enable verbose messages')
-	parser.add_argument('-i', '--indent-type', default='tabs',
-		help='indentation type; tabs or spaces; defaults to tabs')
-	parser.add_argument('-w', '--indent-width', type=int, default=4,
-		help='indentation width; defaults to 4')
-	parser.add_argument('-m', '--max-newlines', type=int, default=2,
-		help='maximum consecutive newlines; defaults to 2')
-	parser.add_argument('-e', '--exclude', nargs='*',
-		help='paths to exclude; simple glob pattern')
-	parser.add_argument('paths', nargs='+',
-		help='directories to (recursively) scan for files')
-	args = vars(parser.parse_args())
+    parser.add_argument('-V', '--verbose', action='store_true',
+                        help='enable verbose messages')
+    parser.add_argument('-i', '--indent-type', default='tabs',
+                        help='indentation type; tabs (default) or spaces')
+    parser.add_argument('-w', '--indent-width', type=int, default=4,
+                        help='indentation width; defaults to 4')
+    parser.add_argument('-m', '--max-newlines', type=int, default=2,
+                        help='maximum consecutive newlines; defaults to 2')
+    parser.add_argument('-e', '--exclude', nargs='*',
+                        help='paths to exclude; simple glob pattern')
+    parser.add_argument('paths', nargs='+',
+                        help='directories to (recursively) scan for files')
+    args = vars(parser.parse_args())
 
-	paths = args['paths']
-	del args['paths']
+    paths = args['paths']
+    del args['paths']
 
-	excludes = args['exclude'] or []
-	del args['exclude']
+    excludes = args['exclude'] or []
+    del args['exclude']
 
-	_verbose = args['verbose']
-	del args['verbose']
+    _verbose = args['verbose']
+    del args['verbose']
 
-	if args['indent_type'].lower().startswith('tab'):
-		args['indent_type'] = 'tabs'
-	elif args['indent_type'].lower().startswith('space'):
-		args['indent_type'] = 'space'
-	else:
-		print("Unknown option `%s' for indent type; valid options are: `tabs' or `spaces'" % args['indent_type'])
-		sys.exit(1)
+    if args['indent_type'].lower().startswith('tab'):
+        args['indent_type'] = 'tabs'
+    elif args['indent_type'].lower().startswith('space'):
+        args['indent_type'] = 'space'
+    else:
+        print("Unknown option `%s' for indent type; valid options are: `tabs' or `spaces'" % args['indent_type'])
+        sys.exit(1)
 
-	allfiles = []
-	for path in paths:
-		if os.path.isfile(path):
-			if do_add_file(path):
-				allfiles.append(path)
-		else:
-			for root, dirs, files in os.walk(path):
-				for f in files:
-					p = '%s/%s' % (root, f)
-					if do_add_file(p.replace(path, '', 1)):
-						allfiles.append(p)
+    allfiles = []
+    for path in paths:
+        if os.path.isfile(path):
+            if do_add_file(path):
+                allfiles.append(path)
+        else:
+            for root, dirs, files in os.walk(path):
+                for f in files:
+                    p = '%s/%s' % (root, f)
+                    if do_add_file(p.replace(path, '', 1)):
+                        allfiles.append(p)
 
-	run(allfiles, **args)
+    run(allfiles, **args)
 
 
 # The MIT License (MIT)
